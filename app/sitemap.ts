a42c55f164392f83fe50b8f961ca7c25b7d355be
xBalbinus <xiangan9@gmail.com>
2024-03-21 21:46:56 -0400

    cleanup minor

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,14 +3,6 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'http://ipfsevents.io',
-      lastModified: new Date(),
-    },
-    {
-      url: 'http://ipfsevents.io/ipfs-friends-cafe',
-      lastModified: new Date(),
-    },
-    {
       url: 'https://2024.ipfs.camp/',
       lastModified: new Date(),
     },
